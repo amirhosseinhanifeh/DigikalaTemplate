27de8e608637b5e3ee24f0ca1024653955ec69c5
hosseinhanifeh <hosseinhanifeh@gmail.com>
2023-10-01 22:35:12 +0330

    fix

--- Ghaleb.Web/Pages/Checkout/Pay.cshtml.cs
+++ Ghaleb.Web/Pages/Checkout/Pay.cshtml.cs
@@ -1,5 +1,7 @@
 using ALO.DataAccessLayer.DataContext;
+using ALO.DomainClasses.Entity.Order;
 using Dto.Payment;
+using Ghaleb.Web.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -21,29 +23,61 @@ namespace Ghaleb.Web.Pages.Checkout
         }
         public async Task<IActionResult> OnGetAsync(long orderId)
         {
-            
-            var isSandbox =Convert.ToBoolean(configuration["PaymentSetting:IsSandbox"]);
+
+            var isSandbox = Convert.ToBoolean(configuration["PaymentSetting:IsSandbox"]);
             var o = await _context.tbl_Orders
-                .Include(x=>x.User)
+                .Include(x => x.OrderStateHistories)
+                .Include(x => x.DeliveryPrice)
+                .Include(x => x.User)
                 .Include(x => x.UserAddress)
                 .Include(x => x.OrderDetails)
                 .ThenInclude(x => x.ProductPriceHistory)
                 .FirstOrDefaultAsync(x => x.Id == orderId);
 
-            var result = await _payment.Request(new DtoRequest
+            if (o.PaymentMethod == PaymentMethod.INTERNET)
             {
-                Mobile = o.UserAddress.Mobile,
-                CallbackUrl = string.Format("{0}://{1}/checkout/finish/"+orderId, Request.Scheme,Request.Host),
-                Description = "خرید",
-                Email = "hosseinhanifeh@gmail.com",
-                Amount = (int)o.OrderDetails.Sum(x => x.Count * x.ProductPriceHistory.GetPrice()),
-                MerchantId = configuration["PaymentSetting:MerchantId"],
-            },isSandbox==true? ZarinPal.Class.Payment.Mode.sandbox :ZarinPal.Class.Payment.Mode.zarinpal);
-            if (result.Status == 100)
-            {
-                return Redirect((isSandbox == true ? configuration["PaymentSetting:SandboxUrl"] : configuration["PaymentSetting:PaymentUrl"]) +result.Authority);
+                var user = await _context.tbl_Users.FindAsync(User.UserId());
+                var result = await _payment.Request(new DtoRequest
+                {
+                    Mobile = o.UserAddress.Mobile,
+                    CallbackUrl = string.Format("{0}://{1}/checkout/finish/" + orderId, Request.Scheme, Request.Host),
+                    Description = "خرید",
+                    Email = user.Email,
+                    Amount = (int)o.TotalPrice(),
+                    MerchantId = configuration["PaymentSetting:MerchantId"],
+                }, isSandbox == true ? ZarinPal.Class.Payment.Mode.sandbox : ZarinPal.Class.Payment.Mode.zarinpal);
+                if (result.Status == 100)
+                {
+                    return Redirect((isSandbox == true ? configuration["PaymentSetting:SandboxUrl"] : configuration["PaymentSetting:PaymentUrl"]) + result.Authority);
+                }
             }
-            return RedirectToPage("Error");
+            else
+            {
+                var wallet = await _context.tbl_Wallets.FirstOrDefaultAsync(h => h.UserId == User.UserId());
+                if (wallet.Cost > o.TotalPrice())
+                {
+                    wallet.Cost -= o.TotalPrice();
+                    o.PaymentMethod = PaymentMethod.WALLET;
+                    o.OrderStateHistories.Add(new tbl_OrderStateHistory
+                    {
+                        OrderState = OrderState.PAYED
+                    });
+
+                    await _context.SaveChangesAsync();
+                    Response.Cookies.Delete("basket", new CookieOptions
+                    {
+                        Expires = DateTime.Now.AddDays(-1)
+                    });
+                    return RedirectToPage("WalletFinish", new { StatusCode = "200" });
+                }
+                else
+                {
+                    return RedirectToPage("WalletFinish", new { StatusCode = "501" });
+
+                }
+
+            }
+            return RedirectToPage("Error", new { statusCode = "-101" });
         }
     }
 }
